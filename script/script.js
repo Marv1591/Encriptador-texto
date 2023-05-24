9c9f542d30cfc59e287027d2b5dc47ba22776974
Marvin Canales <mcanalesv@outlook.com>
2023-05-24 09:22:00 -0600

    Encriptador completo

--- script/script.js
+++ script/script.js
@@ -12,7 +12,7 @@ function encriptar() {
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat");
 
-    if (document.getElementById("texto").value.length != 0) {
+    if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto encriptado con éxito.";
         parrafo.textContent = "";
@@ -21,6 +21,32 @@ function encriptar() {
         muñeco.src = "./img/muñeco.png";
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-            alert("Debes de ingresar algún texto")
+        alert("Debes de ingresar algún texto.")
         }
 }
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/desencriptado.jpg";
+      } else {
+        muñeco.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+      }
+  }
